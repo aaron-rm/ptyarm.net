4cf03ce78e763b83e7d208e159ecdebce616cb5e
Aaron <aaron.remc@gmail.com>
2026-07-07 18:59:51 -0500

    eliminar sesiones

--- js/conexion.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
-const SUPABASE_URL = 'https://jaaagmjjjfhpaqzplslf.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_KM7xJKwITCDVvUjxEN90mg__DhXQbO7'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
